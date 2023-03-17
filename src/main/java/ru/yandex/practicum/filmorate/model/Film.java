38f0a205ad15635b49044face47b1a8fbaddde0a
Staraikers <staraikers123@yandex.ru>
2023-03-17 21:19:49 +0500

    FIX 3

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,14 +3,11 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
-@Data
 @Builder
+@Data
 public class Film {
     private int id;
 
@@ -20,6 +17,8 @@ public class Film {
     @NotNull
     @Size(max = 200)
     private String description;
+
+    @NotNull
     private LocalDate releaseDate;
 
     @Positive
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -4,18 +4,19 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.Builder;
 import lombok.Data;
 
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
-@Data
 @Builder
+@Data
 public class User {
     private int id;
 
-    @Email
     @NotBlank
+    @Email
     private String email;
 
     @NotBlank
